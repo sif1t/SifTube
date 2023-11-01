95e502047d96b82ac69d13a0a5367719e27f84dc
arifeensifat01 <arifeen.sifat01@gmail.com>
2023-11-01 19:45:36 +0600

    sidebar section complite

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 var menuIcon = document.querySelector(".menu-icon");
 var sidebar = document.querySelector(".sidebar");
+var container = document.querySelector(".container");
 
 
 menuIcon.onclick = function(){
-    sidebar.classList.toggle("small-sidebar")
+    sidebar.classList.toggle("small-sidebar");
+    container.classList.toggle("large-container");
 }
